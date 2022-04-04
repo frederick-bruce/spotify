a9208229c7859b3de59cb072798a055d0bc3a748
Frederick Bruce <frederick.bruce.dev@gmail.com>
2022-04-04 14:28:11 -0400

    left nav
    beginning of left navbar

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Sidebar from "../components/Sidebar";
+
+export default function Home() { 
+  return (
+    <div className="">
+      {/* Head */}
+
+      <main>
+        <Sidebar />
+        {/* Center */}
+      </main>
+
+      <div>
+        {/* Player */}
+      </div>
+    </div>
+  )
+}
+
+
